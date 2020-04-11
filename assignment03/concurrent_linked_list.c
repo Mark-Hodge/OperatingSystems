f9805c0baf1897ea91dc38c0d7936bb4b146e36b
Mark <mrh598@msstate.edu>
2020-04-11 12:02:14 -0500

    Added task 1 functionality

--- assignment03/concurrent_linked_list.c
+++ assignment03/concurrent_linked_list.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 #include <pthread.h>
+#include <sys/time.h>
+
+#define LISTSIZE 100
+
+/* typedef struct __list_args {
+	int key[LISTSIZE / 2];
+	void *L;
+} list_args; */
 
 // basic node structure
 typedef struct __node_t {
@@ -20,13 +29,15 @@ void List_Init(list_t *L) {
 }
 
 void List_Insert(list_t *L, int key) {
+	//printf("\t-> List_Insert: begin (thread id = %ld)\n", pthread_self());
+
 	// synchronization not needed
 	node_t *new = malloc(sizeof(node_t));
 	if (new == NULL) {
 		perror("malloc");
 		return;
 	}
-	new->key = key;
+	new->key = rand();
 	
 	// just lock critical section
 	pthread_mutex_lock(&L->lock);
@@ -34,9 +45,11 @@ void List_Insert(list_t *L, int key) {
 	L->head = new;
 	pthread_mutex_unlock(&L->lock);
 	
-	printf("\t-> key: %d next: %p\n", key, new->next);
+	//printf("\t   key: %d next: %p\n", new->key, new->next);
+	
+	//printf("\t   List_Insert: end (thread id = %ld)\n", pthread_self());
 }
-
+/* 
 int List_Lookup(list_t *L, int key) {
 	int rv = -1;
 	pthread_mutex_lock(&L->lock);
@@ -51,44 +64,50 @@ int List_Lookup(list_t *L, int key) {
 	}
 	pthread_mutex_unlock(&L->lock);
 	return rv; // now both success and failure
+} */
+
+void * Task_1(void *L) {
+	printf("\t-> Task_1: begin (thread id = %ld)\n", pthread_self());
+/* 	list_t L;
+	List_Init(&L); */
+	
+	int randomSeed = 0;
+	srand(randomSeed);
+	
+	for (int i = 0; i < (LISTSIZE / 2); i++) {
+		int randomKey = rand();
+		List_Insert(L, randomKey);
+	}
+	
+	printf("\tTask_1: end (thread id = %ld)\n", pthread_self());
+	return NULL;
 }
 
 int main(int argc, char *argv[]) {
-	//pthread_t p1, p2;
-	printf("main: begin\n");
+	printf("main: begin (thread id = %ld)\n", pthread_self());
 	
-	printf("main: list_init\n");
+	struct timeval start1;
+	struct timeval end1;
+	long t1_microsecs;
+	
+	// initialize new linked list structure
 	list_t L;
 	List_Init(&L);
-	printf("\t-> address: %p\n", &L);
 	
-	printf("main: random seed\n");
-	int randomSeed = 0;
-	srand(randomSeed);
-		printf("\t-> %d\n", randomSeed);
-	int arr[10];
-
+	// create two new threads and call List_Insert
+	pthread_t p1, p2;
+	gettimeofday(&start1, NULL);
 	
-	printf("main: generate random keys\n");
-	for (int i = 0; i < 10; i++) {
-		int randomKey = rand();
-		arr[i] = randomKey;
-		printf("\t-> array[%d]: %d\n", i, randomKey);
-	}
+	pthread_create(&p1, NULL, Task_1, &L);
+	pthread_create(&p2, NULL, Task_1, &L);
 	
-/* 	for (int i = 0; i < 10; i++) {
-		printf("%d, ", arr[i]);
-	} printf("\n"); */
+	// wait for the threads to finish and join
+	pthread_join(p1, NULL);
+	pthread_join(p2, NULL);
 	
-	printf("main: loop init  list_insert\n");
-	for (int i = 0; i < 10; i++) {
-		List_Insert(&L, arr[i]);
-	}
+	gettimeofday(&end1, NULL);
 	
-	printf("main: loop init list_lookup\n");
-	for (int i = 0; i < 10; i++) {
-		List_Lookup(&L, arr[i]);
-	}
-	
-	printf("main: end\n");
+	printf("main: end (thread id = %ld)\n", pthread_self());
+	t1_microsecs = (end1.tv_usec - start1.tv_usec);
+	printf("\n\t-------Time of Task 1: %ld microseconds-------\n\n", t1_microsecs);
 }
